fc28ec0ec3d82884bb1e79aabe817463de8d7cc6
Craig Davis <craig-davis@users.noreply.github.com>
2019-10-17 09:48:23 -0700

    Ignore deprecated warning during testing for PHPUnit compat

---
+++ test/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+// In PHP7.4 and PHPUnit 6.5 we get a deprecation error. In order to continue
+// supporting PHP7.1 and PHP7.2 in this project, we're going to ignore those
+// warning for now.
+error_reporting(E_ALL ^ E_DEPRECATED);
